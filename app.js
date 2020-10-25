2c0c802bccad9df755cd36b6fb215355b2c9d174
kidus <kidussolomon2004@gmail.com>
2020-10-25 22:59:02 +0300

    routes created

--- app.js
+++ app.js
@@ -4,11 +4,15 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 
 const PORT = process.env.PORT || 8081;
+
+const user = require('./routes/users');
+const auth = require('./routes/auth');
+
 //  Database connection
 (async () => {
     try {
@@ -19,12 +23,17 @@ const PORT = process.env.PORT || 8081;
             useFindAndModify: false
         });
         console.log('MongoDB Connected...');
-    } catch(error){
+    } catch (error) {
         console.log("Can't connect to Db");
         console.error(error.message);
         process.exit(1);
     }
 })();
+
+
+// Define database routes
+app.use('/api/v1/users', user);
+app.use('/api/v1/auth', auth);
 
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
